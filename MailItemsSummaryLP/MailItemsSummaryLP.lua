eae7b4efebc682da8ff31f5d23d42ed08bd72343
Cullen Daugherty <dakjaniels@outlook.com>
2024-06-09 22:36:28 -0500

    update for gold road

--- MailItemsSummaryLP/MailItemsSummaryLP.lua
+++ MailItemsSummaryLP/MailItemsSummaryLP.lua
@@ -1,41 +1,54 @@
-local _G = _G
-local SCENE_MANAGER = _G.SCENE_MANAGER
-if SCENE_MANAGER == nil then
-  return
-end
-local EVENT_MANAGER = _G.GetEventManager()
-if EVENT_MANAGER == nil then
-  return
-end
+local SCENE_MANAGER = SCENE_MANAGER
+local EVENT_MANAGER = GetEventManager()
 local BAG_BACKPACK = 1
 local LINK_STYLE_DEFAULT = 0
-local ZO_MailSendBodyField = _G.ZO_MailSendBodyField
-local SLASH_COMMANDS = _G.SLASH_COMMANDS
-local GetItemLinkValue = _G.GetItemLinkValue
-local GetItemSellValueWithBonuses = _G.GetItemSellValueWithBonuses
-local GetItemInfo = _G.GetItemInfo
-local GetItemLink = _G.GetItemLink
-local GetBagSize = _G.GetBagSize
-local zround = _G.zo_round
-local ZO_LocalizeDecimalNumber = _G.ZO_LocalizeDecimalNumber
-local zo_roundToNearest = _G.zo_roundToNearest
-local EVENT_ADD_ON_LOADED = _G.EVENT_ADD_ON_LOADED
-local zo_callLater = _G.zo_callLater
+local ZO_MailSendBodyField = ZO_MailSendBodyField
+local SLASH_COMMANDS = SLASH_COMMANDS
+local GetItemLinkValue = GetItemLinkValue
+local GetItemSellValueWithBonuses = GetItemSellValueWithBonuses
+local GetItemInfo = GetItemInfo
+local GetItemLink = GetItemLink
+local GetBagSize = GetBagSize
+local zround = zo_round
+local ZO_LocalizeDecimalNumber = ZO_LocalizeDecimalNumber
+local zo_roundToNearest = zo_roundToNearest
+local EVENT_ADD_ON_LOADED = EVENT_ADD_ON_LOADED
+local zo_callLater = zo_callLater
+local tinsert = table.insert
+local tconcat = table.concat
+
+-- Forward declration.
+--- @type function
+local MISLP
+--- @type function
+local GetItemPrice
+--- @type function
+local OnAddOnLoaded
+
+
+--- @class MailItemsSummaryLP
 local MailItemsSummaryLP = {
-  AddonName = "MailItemsSummaryLP",
-  major = "1",
-  minor = "0",
-  patch = "2",
-  build = "2",
-  current_api = 101038,
-  future_api = 101039,
+  AddonName = "MailItemsSummaryLP";
+  major = "1";
+  minor = "0";
+  patch = "3";
+  build = "4";
+  current_api = 101042;
+  future_api = 101043;
 }
 
-local LibPrice = _G.LibPrice
+local LibPrice = LibPrice
 
-local function GetItemPrice(itemLink, considerCondition, bagId, slotIndex)
+---
+--- @param itemLink string
+--- @param considerCondition boolean
+--- @param bagId Bag
+--- @param slotIndex integer
+--- @return number
+GetItemPrice = function (itemLink, considerCondition, bagId, slotIndex)
   local defaultPrice = GetItemLinkValue(itemLink, considerCondition) or GetItemSellValueWithBonuses(bagId, slotIndex)
   local libPriceValue = LibPrice.ItemLinkToPriceGold(itemLink, "mm", "att", "ttc")
+  --- @type number
   local price
   if libPriceValue then
     price = zround(libPriceValue)
@@ -43,7 +56,7 @@ local function GetItemPrice(itemLink, considerCondition, bagId, slotIndex)
   return price or defaultPrice
 end
 
-local function MISLP()
+MISLP = function ()
   if not SCENE_MANAGER:IsShowing("mailSend") then
     return
   end
@@ -51,39 +64,43 @@ local function MISLP()
   local bagId = BAG_BACKPACK
   local totalprice = zo_roundToNearest(0, 0)
   local linkStyle = LINK_STYLE_DEFAULT
-  for slotIndex = 0, GetBagSize(bagId) - 1 do
+  local bagSlots = GetBagSize(bagId)
+  for slotIndex = 0, bagSlots - 1 do
     local itemLink = GetItemLink(bagId, slotIndex, linkStyle)
-    local _, stack, _, _, locked, _ = GetItemInfo(bagId, slotIndex)
+    local _, stack, _, _, locked, _, _, _, _ = GetItemInfo(bagId, slotIndex)
     if locked then
-      local price = GetItemPrice(itemLink) or 0
+      local price = GetItemPrice(itemLink, false, bagId, slotIndex) or 0
       price = zround(price)
-      table.insert(
-        ptable,
+      tinsert(ptable,
         itemLink
-          .. "x"
-          .. stack
-          .. " "
-          .. ZO_LocalizeDecimalNumber(price)
-          .. "="
-          .. ZO_LocalizeDecimalNumber(stack * price)
+        .. " x"
+        .. stack
+        .. " "
+        .. ZO_LocalizeDecimalNumber(price)
+        .. " = "
+        .. ZO_LocalizeDecimalNumber(stack * price)
       )
+      --- @type number
       totalprice = totalprice + price * stack
     end
   end
-  table.insert(ptable, "")
-  table.insert(ptable, "Total Attached Value = " .. ZO_LocalizeDecimalNumber(totalprice))
-  ZO_MailSendBodyField:SetText(table.concat(ptable, "\n"))
+  tinsert(ptable, "")
+  tinsert(ptable, "Total Attached Value = " .. ZO_LocalizeDecimalNumber(totalprice))
+  ZO_MailSendBodyField:SetText(tconcat(ptable, "\n"))
 end
 
 local WAIT_TO_SLASH = 2000
 
-local function OnAddOnLoaded(_, addonName)
+--- @param _ any
+--- @param addonName string
+OnAddOnLoaded = function (_, addonName)
   if addonName ~= MailItemsSummaryLP.AddonName then
     return
   end
   EVENT_MANAGER:UnregisterForEvent(MailItemsSummaryLP.AddonName, EVENT_ADD_ON_LOADED)
-  zo_callLater(function()
+  zo_callLater(function ()
     SLASH_COMMANDS["/mislp"] = MISLP
   end, WAIT_TO_SLASH)
 end
+
 EVENT_MANAGER:RegisterForEvent(MailItemsSummaryLP.AddonName, EVENT_ADD_ON_LOADED, OnAddOnLoaded)
